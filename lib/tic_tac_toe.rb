09c793d0a8c4ec539620818557b11784e6ef6b6b
Pete Fowler <fowler.pete@gmail.com>
2022-09-23 21:07:53 -0600

    Add class constant WIN_COMBOS

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,9 @@
 class Game 
-  attr_reader :board
-  
-    def initialize 
-      @board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-    end
+  attr_reader :board, :WIN_COMBOS
+
+  WIN_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
+    
+  def initialize 
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+  end
 end
--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -1,10 +1,19 @@
 require_relative '../lib/tic_tac_toe'
 
-RSpec.describe Game do
+describe Game do
+  let(:game) { Game.new }
+
   describe "Game class #initialize" do
     it "creates an array of 9 1 space strings and has an @board attr" do
-      game = Game.new
       expect(game.board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])        
     end
   end
+
+  describe "WIN_COMBOS" do 
+    it "has a class constant of all win combos" do 
+      expect(Game::WIN_COMBOS).to eq([[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]])
+    end
+  end 
+
+  
 end
