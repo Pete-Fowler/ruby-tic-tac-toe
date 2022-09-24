b0b9bcc1cf21dc11f71f5691db3b2053745f842c
Pete Fowler <fowler.pete@gmail.com>
2022-09-23 20:54:38 -0600

    Pass initialize test

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,7 @@
 class Game 
-
+  attr_reader :board
+  
+    def initialize 
+      @board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+    end
 end
--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -3,8 +3,8 @@ require_relative '../lib/tic_tac_toe'
 RSpec.describe Game do
   describe "Game class #initialize" do
     it "creates an array of 9 1 space strings and has an @board attr" do
-      game = Game
-      expect(game.board).to eq(" ", " ", " ", " ", " ", " ", " ", " ", " ")        
+      game = Game.new
+      expect(game.board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])        
     end
   end
 end
